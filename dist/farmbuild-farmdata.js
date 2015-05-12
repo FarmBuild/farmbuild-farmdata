c33d2fd0e947c94657f19261c241e452a7f5838b
gotaheads <gotahiroki@gmail.com>
2015-05-12 16:36:00 +1000

    val moved to core

--- dist/farmbuild-farmdata.js
+++ dist/farmbuild-farmdata.js
@@ -1,6 +1,6 @@
 "use strict";
 
-angular.module("farmbuild.farmdata", []);
+angular.module("farmbuild.farmdata", [ "farmbuild.core" ]);
 
 window.farmbuild = {
     farmdata: {}
@@ -90,38 +90,3 @@ angular.module("farmbuild.farmdata").factory("farmdataSession", function($log, v
 });
 
 "use strict";
-
-angular.module("farmbuild.farmdata").factory("validations", function($log) {
-    var validations = {};
-    validations.isPositiveNumberOrZero = function(value) {
-        return !isNaN(parseFloat(value)) && isFinite(value) && parseFloat(value) >= 0;
-    };
-    validations.isPositiveNumber = function(value) {
-        return validations.isPositiveNumberOrZero(value) && parseFloat(value) > 0;
-    };
-    validations.isAlphabet = function(value) {
-        var regex = /^[A-Za-z]+$/gi;
-        return regex.test(value);
-    };
-    validations.isAlphanumeric = function(value) {
-        var regex = /^[a-zA-Z0-9]*[a-zA-Z]+[a-zA-Z0-9 _]*$/gi;
-        return regex.test(value);
-    };
-    var isEmpty = function(data) {
-        if (typeof data == "number" || typeof data == "boolean") {
-            return false;
-        }
-        if (typeof data == "undefined" || data === null) {
-            return true;
-        }
-        if (typeof data.length != "undefined") {
-            return data.length == 0;
-        }
-        return false;
-    };
-    validations.isEmpty = isEmpty;
-    validations.isDefined = angular.isDefined;
-    validations.isArray = angular.isArray;
-    validations.equals = angular.equals;
-    return validations;
-});
