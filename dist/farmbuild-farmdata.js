1da7f1d0dfa5c45292a5d360343f5837da1f9295
hxg <hiroki.gota@spatialvision.com.au>
2015-05-26 12:42:58 +1000

    added supported crs

--- dist/farmbuild-farmdata.js
+++ dist/farmbuild-farmdata.js
@@ -8,16 +8,17 @@ angular.injector([ "ng", "farmbuild.farmdata" ]);
 
 "use strict";
 
-angular.module("farmbuild.farmdata").factory("farmdata", function($log, farmdataSession, farmdataValidator, validations) {
+angular.module("farmbuild.farmdata").factory("farmdata", function($log, farmdataSession, farmdataValidator, crsSupported, validations) {
     var farmdata = {
         session: farmdataSession,
-        validator: farmdataValidator
+        validator: farmdataValidator,
+        crsSupported: crsSupported
     }, isEmpty = validations.isEmpty, defaults = {
         id: "" + new Date().getTime(),
         name: "My new farm",
         geometry: {
             type: "Polygon",
-            crs: "EPSG:4283",
+            crs: crsSupported[0].name,
             coordinates: []
         }
     }, create = function(name, id) {
@@ -55,6 +56,36 @@ angular.module("farmbuild.farmdata").factory("farmdata", function($log, farmdata
     window.farmbuild.farmdata = farmdata;
     return farmdata;
 });
+
+angular.module("farmbuild.farmdata").constant("crsSupported", [ {
+    label: "GDA 94 Geographics:",
+    name: "EPSG:4283",
+    projection: "+proj=longlat +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +no_defs"
+}, {
+    label: "WGS 84 Geographics:",
+    name: "EPSG:4326",
+    projection: "+proj=longlat +datum=WGS84 +no_defs"
+}, {
+    label: "Web Mercator:",
+    name: "EPSG:3857",
+    projection: "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs"
+}, {
+    label: "VicGrid 94:",
+    name: "EPSG:3111",
+    projection: "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs"
+}, {
+    label: "NSW Lamberts:",
+    name: "EPSG:3308",
+    projection: "+proj=lcc +lat_1=-30.75 +lat_2=-35.75 +lat_0=-33.25 +lon_0=147 +x_0=9300000 +y_0=4500000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
+}, {
+    label: "SA Lamberts:",
+    name: "EPSG:3107",
+    projection: "+proj=lcc +lat_1=-28 +lat_2=-36 +lat_0=-32 +lon_0=135 +x_0=1000000 +y_0=2000000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
+}, {
+    label: "Australian Albers:",
+    name: "EPSG:3577",
+    projection: "+proj=aea +lat_1=-18 +lat_2=-36 +lat_0=0 +lon_0=132 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
+} ]);
 
 "use strict";
 
@@ -122,6 +153,8 @@ angular.module("farmbuild.farmdata").factory("farmdataSession", function($log, $
 
 "use strict";
 
+"use strict";
+
 angular.module("farmbuild.core").factory("farmdataValidator", function(validations, $log) {
     var farmdataValidator = {}, _isDefined = validations.isDefined, _isArray = validations.isArray, _isPositiveNumber = validations.isPositiveNumber, _isPositiveNumberOrZero = validations.isPositiveNumberOrZero, _isEmpty = validations.isEmpty, _isObject = validations.isObject, _isString = validations.isString, areaUnitDefault = "hectare";
     function errorLog() {}
@@ -144,5 +177,3 @@ angular.module("farmbuild.core").factory("farmdataValidator", function(validatio
     farmdataValidator.validate = _validate;
     return farmdataValidator;
 });
-
-"use strict";
